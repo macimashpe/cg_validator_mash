6e2f851fb770d1477725cba0ba42412c3800e8f3
Maci Miri <maci@mashpe.net>
2022-11-18 17:09:53 -0800

    Removed parameters out to .toml file

--- cg_validator_mash/main.py
+++ cg_validator_mash/main.py
@@ -1,40 +1,58 @@
-# from itertools import product
 import math
+import pathlib
+import toml
+import logging
+
+# configure logger for debug messages
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s : %(levelname)s : %(name)s : %(message)s')
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+file_handler = logging.FileHandler(filename="log.log")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
 
 if __name__ == "__main__":
-    # get/calculate all necessary variables
+    # load cfg file with robot parameters
+    cfg_file_path = pathlib.Path(__file__).parent / 'data/cg_parameters_mash.toml'
+    with open(cfg_file_path) as cfg_file:
+        cfg_data = toml.load(cfg_file)
+        logger.debug(f'loaded toml cfg file successfully : {str(cfg_file_path)}')
+
+    # Get/calculate all necessary variables
 
     # need m_fsp2, m_w2, ld250_m, load_m, phi, and CG Z height to calc max_accel
-    # TODO: move all these to external file
-    # all below pysical definitions are taken from karthick's code (...DO_NOT_DELETE.py)
-    caster_swivel = 32		#(mm)
-    platform_cg_x = 0			#(mm)
-    platform_cg_y = -44.9		#-24.4;    %(mm)
-    platform_cg_z = 178.3		#127.8;    %(mm)
-    platform_mass = 142			#(kg)
-    front_caster_x = 232.5			#(mm) x-coordinate of the Swivel point of front caster   
-    front_caster_y = 285			#(mm) y-coordinate of the Swivel point of front caster  
+    platform = cfg_data['platform']['type']
+    caster_swivel = cfg_data[platform]['dimensions']['caster_swivel']
+    platform_cg_x = cfg_data[platform]['dimensions']['platform_cg_x']
+    platform_cg_y = cfg_data[platform]['dimensions']['platform_cg_y']
+    platform_cg_z = cfg_data[platform]['dimensions']['platform_cg_z']
+    platform_mass = cfg_data[platform]['dimensions']['mass']
+    front_caster_x = cfg_data[platform]['dimensions']['front_caster_x']
+    front_caster_y = cfg_data[platform]['dimensions']['front_caster_y']
     front_caster_y_minus = front_caster_y-caster_swivel		#(mm) wheel behind the caster mounting point (moving forward)
     front_caster_y_plus = front_caster_y+caster_swivel		#(mm) wheel in front of caster mounting point(moving backward)
-    rear_caster_x = 232.5		#(mm)x-coordinate of the Swivel point of rear caster 
-    rear_caster_y = -285		#(mm) y-coordinate of the Swivel point of rear caster
+    rear_caster_x = cfg_data[platform]['dimensions']['rear_caster_x']
+    rear_caster_y = cfg_data[platform]['dimensions']['rear_caster_y']
     rear_caster_y_minus = rear_caster_y-caster_swivel		#(mm) wheel behind the caster mounting point (moving forward)
     rear_caster_y_plus= rear_caster_y+caster_swivel		#(mm) wheel in front of caster mounting point(moving backward)
-    drive_wheel_x = 301.8		#(mm)
-    drive_wheel_y = 0			#(mm)
+    drive_wheel_x = cfg_data[platform]['dimensions']['drive_wheel_x']
+    drive_wheel_y = cfg_data[platform]['dimensions']['drive_wheel_y']
 
-    payload_mass = 50 #  [kg]
-    payload_cg_x = 0
-    payload_cg_y = 5
-    payload_cg_z = 400
+    payload_mass = cfg_data['payload']['mass']
+    payload_cg_x = cfg_data['payload']['cg']['x']
+    payload_cg_y = cfg_data['payload']['cg']['y']
+    payload_cg_z = cfg_data['payload']['cg']['z']
 
     # Incline angle (rad), representing the ramp on the floor. If using FL of 15, the angle would be about 0.5 degrees
     # psi = 0.5*math.pi/180
-    psi = 0
+    psi = cfg_data['misc']['psi']
     # ramp angle, not to be confused with psi which represenets minor out of levelness of the floor
     # phi is used to determine transverse tipping when ld250 is sideways on the ramp
     # phi = 3*math.pi/180 	# this is lower than the 5degree used for wheel chair ramps
-    phi = 0
+    phi = cfg_data['misc']['phi']
     # angle of of the FO axis relative to the center line of ld250 parallel to y-axis(rad)
     th = math.atan(abs((rear_caster_x-front_caster_x)/(front_caster_y_minus-rear_caster_y_minus))) # th=0 for Ld250, becasue casters are inline
 
@@ -68,8 +86,8 @@ if __name__ == "__main__":
 
     # calc velocity_max
 
-    print(f'accel_max: {accel_max}')
-    print(f'decel_max: {decel_max}')
+    logger.debug(f'accel_max: {accel_max}')
+    logger.debug(f'decel_max: {decel_max}')
     pass
 
 
