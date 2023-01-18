7d475de496df074da4fd518fd8a22715a6742b34
Maci Miri <maci@mashpe.net>
2023-01-17 16:21:02 -0800

    Remove model matrices from toml config.

--- src/cg_validator_mash/matrix_main.py
+++ src/cg_validator_mash/matrix_main.py
@@ -22,38 +22,11 @@ def main():
     with open(cfg_file_path) as cfg_file:
         cfg_data = toml.load(cfg_file)
         logger.debug(f'toml config loaded')
-    
-    # load robot characteristic matrix from cfg file
-    ld250_normal_drive_Mk = cfg_data['matrix_models']['ld250_normal_drive']
-    logger.debug(ld250_normal_drive_Mk)
-    eval_global_vars = {'__builtins__':{},'sin': sin, 'cos': cos}
-    eval_local_vars = {'Br1': 1,
-                        'Br2': 1,
-                        'Dc': 1,
-                        'r': 1,
-                        'x': 1,
-                        'y': 1,
-                        'Bf1': 1,
-                        'Bf2': 1,
-                        'Dd': 1,
-                        'w': 1,
-                        'h': 1,
-                        'dir': 1,
-                        'u': 1,
-                        'M': 1,
-                        'g': 1,
-                        'Jz': 1,
-                        'theta': 1,
-                        'L': 1,
-                        'R': 1,
-                        'ax': 1
-                        }
-    ld250_brake_drive_Mk_eval = [[eval(item, eval_global_vars, eval_local_vars) for item in row] for row in ld250_normal_drive_Mk]
-    logger.debug(ld250_brake_drive_Mk_eval)
 
-    md_robot = Model_MD.MD(650)
-    ld_robot = Model_LD250.LD250()
-    pass
+    # init robot object from model libraries
+    ld_mass = cfg_data['LD']['dimensions']['mass']
+    ld_robot = Model_LD250.LD250(ld_mass)
+    # next step is to solve caster angles (taken from omrom example)
 
 if __name__ == "__main__":
     main()
